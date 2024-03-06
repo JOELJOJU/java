41be8a171bb312e79ec9b7962e71f51e34cd779c
JOELJOJU <144875016+JOELJOJU@users.noreply.github.com>
2024-03-06 11:16:35 +0530

    complexnum.java

---
+++ complexnum.java
@@ -0,0 +1,31 @@
+public class complexnum 
+{
+  double real;
+  double img;
+  
+  complexnum (double r,double i)
+ {
+    this.real=r;
+    this.img=i;
+ }
+ complexnum(){}
+
+ complexnum sum(complexnum c1,complexnum c2)
+ {
+
+   complexnum temp=new complexnum();
+
+   temp.real=c1.real+c2.real;
+   temp.img=c1.img+c2.img;
+   return temp;
+ }
+  public static void main(String args[])
+ {
+
+   complexnum c1=new complexnum (2.3,5.2);
+   complexnum c2=new complexnum (3.2,3.5);
+   complexnum temp1=new complexnum();
+   temp1=temp1.sum(c1,c2);
+   System.out.println("Sum is:"+temp1.real+"+i"+temp1.img);
+ }
+}
